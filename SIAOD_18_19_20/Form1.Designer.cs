c230b10f67e6141246f4d4b221400181b77e8fb8
gfedorov7 <fedorov.programming@gmail.com>
2026-03-12 23:51:20 +0300

    laba 18

---
+++ SIAOD_18_19_20/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace SIAOD_18_19_20;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        addNew = new System.Windows.Forms.Button();
+        clearQueue = new System.Windows.Forms.Button();
+        getMax = new System.Windows.Forms.Button();
+        createQueue = new System.Windows.Forms.Button();
+        newNum = new System.Windows.Forms.NumericUpDown();
+        changePriority = new System.Windows.Forms.Button();
+        label1 = new System.Windows.Forms.Label();
+        numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+        label2 = new System.Windows.Forms.Label();
+        numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+        label3 = new System.Windows.Forms.Label();
+        dataGridView1 = new System.Windows.Forms.DataGridView();
+        label4 = new System.Windows.Forms.Label();
+        dataGridView2 = new System.Windows.Forms.DataGridView();
+        dataGridView3 = new System.Windows.Forms.DataGridView();
+        label5 = new System.Windows.Forms.Label();
+        exit = new System.Windows.Forms.Button();
+        ((System.ComponentModel.ISupportInitialize)newNum).BeginInit();
+        ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+        ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
+        ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+        ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+        ((System.ComponentModel.ISupportInitialize)dataGridView3).BeginInit();
+        SuspendLayout();
+        // 
+        // addNew
+        // 
+        addNew.Location = new System.Drawing.Point(12, 47);
+        addNew.Name = "addNew";
+        addNew.Size = new System.Drawing.Size(251, 23);
+        addNew.TabIndex = 0;
+        addNew.Text = "Вставить новый";
+        addNew.UseVisualStyleBackColor = true;
+        addNew.Click += addNew_Click;
+        // 
+        // clearQueue
+        // 
+        clearQueue.Location = new System.Drawing.Point(269, 12);
+        clearQueue.Name = "clearQueue";
+        clearQueue.Size = new System.Drawing.Size(267, 26);
+        clearQueue.TabIndex = 1;
+        clearQueue.Text = "Очистить очередь";
+        clearQueue.UseVisualStyleBackColor = true;
+        clearQueue.Click += clearQueue_Click;
+        // 
+        // getMax
+        // 
+        getMax.Location = new System.Drawing.Point(542, 12);
+        getMax.Name = "getMax";
+        getMax.Size = new System.Drawing.Size(251, 26);
+        getMax.TabIndex = 2;
+        getMax.Text = "Извлечь наибольший";
+        getMax.UseVisualStyleBackColor = true;
+        getMax.Click += getMax_Click;
+        // 
+        // createQueue
+        // 
+        createQueue.Location = new System.Drawing.Point(12, 12);
+        createQueue.Name = "createQueue";
+        createQueue.Size = new System.Drawing.Size(251, 26);
+        createQueue.TabIndex = 3;
+        createQueue.Text = "Создать очередь";
+        createQueue.UseVisualStyleBackColor = true;
+        createQueue.Click += createQueue_Click;
+        // 
+        // newNum
+        // 
+        newNum.Location = new System.Drawing.Point(269, 47);
+        newNum.Minimum = new decimal(new int[] { 10, 0, 0, 0 });
+        newNum.Name = "newNum";
+        newNum.Size = new System.Drawing.Size(63, 23);
+        newNum.TabIndex = 4;
+        newNum.Value = new decimal(new int[] { 10, 0, 0, 0 });
+        // 
+        // changePriority
+        // 
+        changePriority.Location = new System.Drawing.Point(338, 46);
+        changePriority.Name = "changePriority";
+        changePriority.Size = new System.Drawing.Size(198, 24);
+        changePriority.TabIndex = 5;
+        changePriority.Text = "Изменить приоритет";
+        changePriority.UseVisualStyleBackColor = true;
+        changePriority.Click += changePriority_Click;
+        // 
+        // label1
+        // 
+        label1.Location = new System.Drawing.Point(542, 46);
+        label1.Name = "label1";
+        label1.Size = new System.Drawing.Size(46, 24);
+        label1.TabIndex = 6;
+        label1.Text = "c (idx)";
+        // 
+        // numericUpDown2
+        // 
+        numericUpDown2.Location = new System.Drawing.Point(594, 46);
+        numericUpDown2.Name = "numericUpDown2";
+        numericUpDown2.Size = new System.Drawing.Size(58, 23);
+        numericUpDown2.TabIndex = 7;
+        // 
+        // label2
+        // 
+        label2.Location = new System.Drawing.Point(658, 46);
+        label2.Name = "label2";
+        label2.Size = new System.Drawing.Size(66, 23);
+        label2.TabIndex = 8;
+        label2.Text = "на (value)";
+        // 
+        // numericUpDown3
+        // 
+        numericUpDown3.Location = new System.Drawing.Point(730, 46);
+        numericUpDown3.Name = "numericUpDown3";
+        numericUpDown3.Size = new System.Drawing.Size(58, 23);
+        numericUpDown3.TabIndex = 9;
+        // 
+        // label3
+        // 
+        label3.Location = new System.Drawing.Point(16, 82);
+        label3.Name = "label3";
+        label3.Size = new System.Drawing.Size(776, 34);
+        label3.TabIndex = 10;
+        label3.Text = "Представление в виде массива";
+        label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // dataGridView1
+        // 
+        dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+        dataGridView1.ColumnHeadersHeight = 20;
+        dataGridView1.Location = new System.Drawing.Point(16, 119);
+        dataGridView1.Name = "dataGridView1";
+        dataGridView1.Size = new System.Drawing.Size(752, 32);
+        dataGridView1.TabIndex = 11;
+        // 
+        // label4
+        // 
+        label4.Location = new System.Drawing.Point(16, 165);
+        label4.Name = "label4";
+        label4.Size = new System.Drawing.Size(776, 34);
+        label4.TabIndex = 12;
+        label4.Text = "Представление в виде дерева";
+        label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+        // 
+        // dataGridView2
+        // 
+        dataGridView2.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+        dataGridView2.ColumnHeadersHeight = 20;
+        dataGridView2.Location = new System.Drawing.Point(16, 193);
+        dataGridView2.Name = "dataGridView2";
+        dataGridView2.Size = new System.Drawing.Size(752, 122);
+        dataGridView2.TabIndex = 13;
+        // 
+        // dataGridView3
+        // 
+        dataGridView3.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+        dataGridView3.ColumnHeadersHeight = 20;
+        dataGridView3.Location = new System.Drawing.Point(16, 355);
+        dataGridView3.Name = "dataGridView3";
+        dataGridView3.Size = new System.Drawing.Size(752, 32);
+        dataGridView3.TabIndex = 14;
+        // 
+        // label5
+        // 
+        label5.Location = new System.Drawing.Point(16, 318);
+        label5.Name = "label5";
+        label5.Size = new System.Drawing.Size(776, 34);
+        label5.TabIndex = 15;
+        label5.Text = "Результат выборки";
+        label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // exit
+        // 
+        exit.Location = new System.Drawing.Point(308, 404);
+        exit.Name = "exit";
+        exit.Size = new System.Drawing.Size(198, 24);
+        exit.TabIndex = 16;
+        exit.Text = "Выход";
+        exit.UseVisualStyleBackColor = true;
+        exit.Click += exit_Click;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        BackColor = System.Drawing.SystemColors.Control;
+        ClientSize = new System.Drawing.Size(800, 450);
+        Controls.Add(exit);
+        Controls.Add(label5);
+        Controls.Add(dataGridView3);
+        Controls.Add(dataGridView2);
+        Controls.Add(label4);
+        Controls.Add(dataGridView1);
+        Controls.Add(label3);
+        Controls.Add(numericUpDown3);
+        Controls.Add(label2);
+        Controls.Add(numericUpDown2);
+        Controls.Add(label1);
+        Controls.Add(changePriority);
+        Controls.Add(newNum);
+        Controls.Add(createQueue);
+        Controls.Add(getMax);
+        Controls.Add(clearQueue);
+        Controls.Add(addNew);
+        Location = new System.Drawing.Point(15, 15);
+        Text = "Федоров 24вп1";
+        ((System.ComponentModel.ISupportInitialize)newNum).EndInit();
+        ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+        ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
+        ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+        ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+        ((System.ComponentModel.ISupportInitialize)dataGridView3).EndInit();
+        ResumeLayout(false);
+    }
+
+    private System.Windows.Forms.DataGridView dataGridView3;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.Button exit;
+
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.DataGridView dataGridView2;
+
+    private System.Windows.Forms.Button addNew;
+    private System.Windows.Forms.Button clearQueue;
+    private System.Windows.Forms.Button getMax;
+    private System.Windows.Forms.Button createQueue;
+    private System.Windows.Forms.NumericUpDown newNum;
+    private System.Windows.Forms.Button changePriority;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.NumericUpDown numericUpDown2;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.NumericUpDown numericUpDown3;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.DataGridView dataGridView1;
+
+    #endregion
+}
---
+++ SIAOD_18_19_20/Form1.cs
@@ -0,0 +1,241 @@
+namespace SIAOD_18_19_20;
+
+public partial class Form1 : Form
+{
+    private const int DATAGRID1_3_ROWS = 1;
+    private const int DATAGRID2_ROWS = 4;
+    private const int DATAGRID_COLUMNS = 15;
+    private const int WIDTH = 50;
+    private const int HEIGHT = 30;
+    private const int MAX_ELEMENTS = 15;
+
+
+    private int[] A;
+    private Random rnd;
+    
+    public Form1()
+    {
+        InitializeComponent();
+     
+        dataGridView1.ColumnHeadersVisible = false;
+        dataGridView1.RowHeadersVisible = false;
+        dataGridView1.RowCount = DATAGRID1_3_ROWS;
+        dataGridView1.ColumnCount = DATAGRID_COLUMNS;
+
+        dataGridView1.ScrollBars = ScrollBars.None;
+        
+        dataGridView1.ReadOnly = true;
+        dataGridView1.Rows[0].Height = HEIGHT;
+        for (int i = 0; i < DATAGRID_COLUMNS; i++)
+        {
+            dataGridView1.Columns[i].Width = WIDTH;
+        }
+        
+        dataGridView2.ColumnHeadersVisible = false;
+        dataGridView2.RowHeadersVisible = false;
+        dataGridView2.RowCount = DATAGRID2_ROWS;
+        dataGridView2.ColumnCount = DATAGRID_COLUMNS;
+        dataGridView2.ScrollBars = ScrollBars.None;
+        dataGridView2.ReadOnly = true;
+        for (int i = 0; i < DATAGRID_COLUMNS; i++)
+        {
+            dataGridView2.Columns[i].Width = WIDTH;
+        }
+        for (int i = 0; i < DATAGRID2_ROWS; i++)
+        {
+            dataGridView2.Rows[i].Height = HEIGHT;
+        }
+
+        dataGridView3.ColumnHeadersVisible = false;
+        dataGridView3.RowHeadersVisible = false;
+        dataGridView3.RowCount = DATAGRID1_3_ROWS;
+        dataGridView3.ColumnCount = DATAGRID_COLUMNS;
+        dataGridView3.ScrollBars = ScrollBars.None;
+        dataGridView3.ReadOnly = true;
+        dataGridView3.Rows[0].Height = HEIGHT;
+        for (int i = 0; i < DATAGRID_COLUMNS; i++)
+        {
+            dataGridView3.Columns[i].Width = WIDTH;
+        }
+        
+        A = new int[MAX_ELEMENTS];
+        rnd = new Random();
+    }
+
+    private void exit_Click(object sender, EventArgs e)
+    {
+        Close();
+    }
+
+    private void createQueue_Click(object sender, EventArgs e)
+    {
+        for (int i = 0; i < MAX_ELEMENTS; i++)
+        {
+            A[i] = rnd.Next(10, 100);
+        }
+
+        for (int i = MAX_ELEMENTS / 2 - 1; i >= 0; i--)
+            heapDown(i, MAX_ELEMENTS);
+
+        clearTabs();
+        ShowArrayAndQueue();
+    }
+
+    private void ShowArrayAndQueue()
+    {
+        for (int i = 0; i < MAX_ELEMENTS; i++)
+        {
+            dataGridView1.Rows[0].Cells[i].Value = A[i];
+        }
+        Print();
+    }
+    
+    private void Print()
+    {
+        int index = 0;
+        for (int level = 0; level < 4; level++)
+        {
+            int elements = (int)Math.Pow(2, level);
+            int step = 8 >> level;
+            int col = step - 1;
+
+            for (int i = 0; i < elements && index < 15; i++)
+            {
+                if (A[index] != 0)
+                    dataGridView2.Rows[level].Cells[col].Value = A[index];
+
+                col += step * 2;
+                index++;
+            }
+        }
+    }
+
+    private void getMax_Click(object sender, EventArgs e)
+    {
+        int last = findLastIndexElement();
+        if (last < 0) return;
+
+        int max = A[0];
+
+        for (int i = 0; i < MAX_ELEMENTS; i++)
+        {
+            if (dataGridView3.Rows[0].Cells[i].Value == null)
+            {
+                dataGridView3.Rows[0].Cells[i].Value = max;
+                break;
+            }
+        }
+
+        A[0] = A[last];
+        A[last] = 0;
+
+        heapDown(0, last);
+
+        clearTabs();
+        ShowArrayAndQueue();
+    }
+
+    private int findLastIndexElement()
+    {
+        for (int i = MAX_ELEMENTS-1; i >= 0; i--)
+        {
+            if (A[i] == 0)
+                continue;
+            return i;
+        }
+
+        return -1;
+    }
+
+    private void clearTabs()
+    {
+        for (int r = 0; r < DATAGRID2_ROWS; r++)
+            for (int c = 0; c < DATAGRID_COLUMNS; c++)
+                dataGridView2.Rows[r].Cells[c].Value = null;
+    }
+
+    private void clearQueue_Click(object sender, EventArgs e)
+    {
+        clearTabs();
+        for (int i = 0; i < MAX_ELEMENTS; i++)
+            dataGridView3.Rows[0].Cells[i].Value = null;
+        for (int i = 0; i < MAX_ELEMENTS; i++)
+            dataGridView1.Rows[0].Cells[i].Value = null;
+    }
+
+    private void addNew_Click(object sender, EventArgs e)
+    {
+        int lastIndex = findLastIndexElement();
+
+        if (lastIndex == MAX_ELEMENTS - 1)
+            return;
+
+        int newIndex = lastIndex + 1;
+
+        A[newIndex] = (int)newNum.Value;
+
+        heapUp(newIndex);
+
+        ShowArrayAndQueue();
+
+        for (int i = MAX_ELEMENTS - 1; i >= 0; i--)
+            if (dataGridView3.Rows[0].Cells[i].Value != null)
+            {
+                dataGridView3.Rows[0].Cells[i].Value = null;
+                break;
+            }
+    }
+    
+    private void heapUp(int k)
+    {
+        while (k > 0 && A[(k - 1) / 2] < A[k])
+        {
+            (A[k], A[(k - 1) / 2]) = (A[(k - 1) / 2], A[k]);
+
+            k = (k - 1) / 2;
+        }
+    }
+
+    private void heapDown(int k, int N)
+    {
+        while (2 * k + 1 < N)
+        {
+            int j = 2 * k + 1;
+
+            if (j + 1 < N && A[j] < A[j + 1])
+                j++;
+
+            if (A[k] >= A[j])
+                break;
+
+            (A[k], A[j]) = (A[j], A[k]);
+            k = j;
+        }
+    }
+
+    private void changePriority_Click(object sender, EventArgs e)
+    {
+        int index = (int)numericUpDown2.Value;
+        int newPriority = (int)numericUpDown3.Value;
+
+        if (index < 0 || index >= MAX_ELEMENTS)
+            return;
+
+        if (A[index] == 0)
+            return;
+
+        int oldPriority = A[index];
+
+        A[index] = newPriority;
+
+        int last = findLastIndexElement() + 1;
+
+        if (newPriority > oldPriority)
+            heapUp(index);
+        else
+            heapDown(index, last);
+
+        clearTabs();
+        ShowArrayAndQueue();
+    }
+}
